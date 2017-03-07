c1384fa9e8b82d956c77405f9d77fe71cd306160
globalusName <emailiukas@gmail.com>
2017-03-07 13:15:56 +0200

    added Quadratic constructor

---
+++ src/calc/QuadraticEquation.java
@@ -0,0 +1,13 @@
+package calc;
+
+public class QuadraticEquation {
+	private int a;
+	private int b;
+	private int c;
+	
+	public QuadraticEquation(int a, int b, int c){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+}
