2f4100dee728aae0298782d7cc206bf449a256f6
andrius.satkauskas <andrius.satkauskas@gmail.com>
2017-03-09 20:24:49 +0200

    added comment for public methods in calc class, not finished yet

--- src/calc/Calculator.java
+++ src/calc/Calculator.java
@@ -230,28 +230,28 @@ public class Calculator implements ActionListener {
 	}
 
 	public static void calculateSin(double a) {
-		// Convert degrees to radian
+		// Convert degrees to radian ye
 		a = Math.toRadians(a);
 		System.out.println("Sin: " + Math.sin(a));
 
 	}
 
 	public static void calculateCos(double a) {
-		// Convert degrees to radian
+		// Convert degrees to radian ye 
 		a = Math.toRadians(a);
 		System.out.println("Cos: " + Math.cos(a));
 
 	}
 
 	public static void calculateTn(double a) {
-		// Convert degrees to radian
+		// Convert degrees to radian ye 
 		a = Math.toRadians(a);
 		System.out.println("Tng: " + Math.tan(a));
 
 	}
 
 	public static void calculateCtn(double a) {
-		// Convert degrees to radian
+		// Convert degrees to radian ye
 		a = Math.toRadians(a);
 		System.out.println("Ctg: " + 1 / Math.tan(a));
 
